86cdc5a3edbdd492e0427dbee0441daaef1dff4d
Naitik Shah <naitik.shah@sakec.ac.in>
2025-03-05 15:07:24 -0500

    It works, importing the correct library

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import sqlite3
-import os
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
-from google import genai
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY") 
-
-client = genai.Client(api_key="GEMINI_API_KEY")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=["How does AI work?"])
-print(response.text)
+import google.generativeai as genai
