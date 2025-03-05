830e676b1fcbb978663e12027302bd5f6ff950a6
Naitik Shah <naitik.shah@sakec.ac.in>
2025-03-05 18:11:57 -0500

    Added check for blank API keys

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if GEMINI_API_KEY is None:
+    print("Gemini API key not found, please refer to the GitHub repo documentation to set up your .env correctly!")
+    sys.exit(1)
+elif TELEGRAM_BOT_TOKEN is None:
+    print("Telegram API key not found, please refer to the GitHub repo documentation to set up your .env correctly!")
+    sys.exit(1)
+else:
+    None
 
 # Configure the API key (this is correct for google-generativeai package)
 genai.configure(api_key=GEMINI_API_KEY)
